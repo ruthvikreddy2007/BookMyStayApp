ef0937d8e28159eff51a901680d5509498fbab06
ruthvikreddy2007 <ruthvikreddy2007@gmail.com>
2026-03-24 14:06:14 +0530

    Base application setup

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        // Application details
+        String appName = "Book My Stay";
+        String version = "v1.0";
+
+        // Welcome message
+        System.out.println("====================================");
+        System.out.println(" Welcome to " + appName);
+        System.out.println(" Version: " + version);
+        System.out.println("====================================");
+        System.out.println("Your smart hotel booking solution starts here!");
+
+
+    }
+}
